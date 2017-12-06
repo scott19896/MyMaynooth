88bd4a9e5fd4385d03f895794a3622d3b41eccc7
romakarol <32648346+romakarol@users.noreply.github.com>
2017-12-06 10:56:06 +0000

    Competitor view table

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿ var rootRef=firebase.database().ref().child("Events");
+
+ rootRef.on("child_added", snap => {
+  
+  var provider=snap.child("provider".val());
+  var name=snap.child("name".val());
+  var description=snap.child("description".val());
+  var ARTS=snap.child("ARTS".val());
+  var CSSE=snap.child("CSSE".val());
+  var OTHER=snap.child("OTHER".val());
+
+  $("#table_body").append("<tr><td>"+provider+"</td></td>"+name+
+                      "</td><td>"+description+"</td><td>"+ARTS+"</td><td>"+CSSE+"</td><td>"+OTHER+"</td></tr>");
+
+  });
+
