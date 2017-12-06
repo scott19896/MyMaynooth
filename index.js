549004c17f2eccd566e9611abcccfa06f7e809e3
romakarol <32648346+romakarol@users.noreply.github.com>
2017-12-06 11:22:25 +0000

    Changed offers page to show both tables

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-﻿ var rootRef=firebase.database().ref().child("Events");
-
+﻿ var rootRef=firebase.database().ref().child("Events/Entertainment");
+ var rootRef2=firebase.database().ref().child("Events/Food");
+ 
  rootRef.on("child_added", snap => {
   
   var provider=snap.child("provider".val());
@@ -8,9 +9,23 @@
   var ARTS=snap.child("ARTS".val());
   var CSSE=snap.child("CSSE".val());
   var OTHER=snap.child("OTHER".val());
+  
+   $("#table_body").append("<tr><td>"+provider+"</td></td>"+name+
+                      "</td><td>"+description+"</td><td>"+ARTS+"</td><td>"+CSSE+"</td><td>"+OTHER+"</td></tr>");
+
+  });
+
+
+ rootRef2.on("child_added", snap => {
+  
+  
+  var provider=snap.child("provider".val());
+  var name=snap.child("name".val());
+  var description=snap.child("description".val());
+
 
   $("#table_body").append("<tr><td>"+provider+"</td></td>"+name+
-                      "</td><td>"+description+"</td><td>"+ARTS+"</td><td>"+CSSE+"</td><td>"+OTHER+"</td></tr>");
+                      "</td><td>"+description+"</td></tr>");
 
   });
 
