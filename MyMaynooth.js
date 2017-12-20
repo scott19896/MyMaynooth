b361e0eb6a085e1b06e26c98cc6378d53b786cc9
scott19896 <scott_19896@yahoo.com>
2017-12-20 12:06:57 +0000

    Add files via upload
    All updated app files-v2

--- MyMaynooth.js
+++ MyMaynooth.js
@@ -8,6 +8,17 @@ $(function() {
     messagingSenderId: "508311719577"
   };
   //firebase.initializeApp(config);
+    document.getElementById("ButStud").addEventListener("click",r =>{
+       $("#CustLog").css("display","block");
+        
+        $("#BusLog").css("display","none");
+       });
+ 
+document.getElementById("ButBus").addEventListener("click",r =>{
+       $("#CustLog").css("display","none");
+        
+        $("#BusLog").css("display","block");
+       });
     $(".js-form").on("submit", event => {
     event.preventDefault();
     const email = $("#js-email").val();
@@ -28,6 +39,38 @@ $(function() {
           },function(error){
             if(error)
                 console.log(error);
+            else
+                window.location.replace("homeMyMaynooth.html")
+            
+        });
+      })
+      .catch(error => {
+        $(".incorrect").css("display", "block");
+      });
+    
+  });
+    $(".js-formBus").on("submit", event => {
+    event.preventDefault();
+    const email = $("#js-email2").val();
+    const password = $("#js-password2").val();
+    const message = " Sign In Successful";
+    var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    console.log(email, password);
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(user => {
+        $(".incorrect").css("display", "none");
+        firebase.database().ref("testSignIn").push({
+            email,
+            message,
+            time,
+          },function(error){
+            if(error)
+                console.log(error);
+            else
+                window.location.replace("BusinessHome.html")
             
         });
       })
